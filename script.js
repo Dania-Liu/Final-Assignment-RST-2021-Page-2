cf73c92c6504a11861571b6de8e5f7ebd76d161a
Dania <>
2021-04-08 15:23:29 +0000

    Fixed ID

--- script.js
+++ script.js
@@ -2,21 +2,21 @@
 let firstAnswer = 0
 
 // Connects button that allows users to submit their answers to JavaScript.
-document.getElementById('firstSubmit').addEventListener('click', firstSubmit)
+document.getElementById('first-submit').addEventListener('click', firstSubmit)
 // Connects button that allows users to reveal a hint to JavaScript.
-document.getElementById('firstHint').addEventListener('click', firstHint)
+document.getElementById('first-hint').addEventListener('click', firstHint)
 
 // Creates function that activates when the user clicks on "Submit" for the first riddle.
 function firstSubmit () {
-  firstAnswer = document.getElementById('firstAnswer').value
+  firstAnswer = document.getElementById('first-answer').value
   if (firstAnswer === 'rainbow') {
-    document.getElementById('firstOutcome').innerHTML = ('Correct, please proceed.')
+    document.getElementById('first-outcome').innerHTML = ('Correct, please proceed.')
   } else {
-    document.getElementById('firstOutcome').innerHTML = ('Incorrect, try again.')
+    document.getElementById('first=outcome').innerHTML = ('Incorrect, try again.')
   }
 }
 
 // Creates function that reveals an image when the user clicks on "Hint" for the first riddle.
 function firstHint () {
-  document.getElementById('firstPicture').style.display = 'block'
+  document.getElementById('first-picture').style.display = 'block'
 }
